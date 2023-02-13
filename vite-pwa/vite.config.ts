889cfcb7f859534b8444cf9c2e7433aa15e013a8
chalz777 <charlesnam37@hotmail.com>
2023-02-13 15:36:04 -0800

    base

--- vite-pwa/vite.config.ts
+++ vite-pwa/vite.config.ts
@@ -122,11 +122,7 @@ export default defineConfig({
         },   
         plugins: [
           vue(),
-          VitePWA(pwaOptions),   
-          //vitePluginWrapper(),
-          outputPluginStats(),
-          requestAnalytics(),
-          hotUpdateReport(),       
+          VitePWA(pwaOptions),            
           replace(replaceOptions), 
         ],
        
